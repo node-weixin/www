41b6388235b463d2c494c2406cd2a070649a1a60
calidion <calidion@gmail.com>
2016-01-22 03:01:31 +0800

    enabled forum

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,11 @@
  * Controller of the wwwApp
  */
 angular.module('wwwApp')
-  .controller('MainCtrl', function($scope, projects, express) {
+  .controller('MainCtrl', function($scope, projects, express, forum) {
     $scope.projects = projects.data;
     $scope.express = express.data;
+    forum.get(function(res) {
+      console.log(res.data.data);
+      $scope.posts = res.data.data;
+    });
   });
