b0f5f4a01d8be1336c4f572af3cd2af8cc783edf
calidion <calidion@gmail.com>
2016-05-26 21:39:39 +0800

    updated

--- app/scripts/filters/ios.js
+++ app/scripts/filters/ios.js
@@ -10,26 +10,27 @@
  */
 angular.module('wwwApp')
   .filter('ios', function () {
-    return function (input, type) {
+    return function (input, type, tz) {
       var time = new Date(input);
+      tz = tz || 'Asia/Shanghai';
       moment.locale('zh-CN');
       switch (type) {
         case 'arrival':
           var today = new Date();
-          var ymdIn = moment(time).format('YYYY-MM-DD');
-          var ymdNow = moment(today).format('YYYY-MM-DD');
+          var ymdIn = moment(time).tz(tz).format('YYYY-MM-DD');
+          var ymdNow = moment(today).tz(tz).format('YYYY-MM-DD');
           if (ymdIn === ymdNow) {
-            return '今天' + moment(time).format(' hh:mm');
+            return '今天' + moment(time).tz(tz).format(' hh:mm');
           } else {
-            return moment(time).format('DD日 hh:mm');
+            return moment(time).tz(tz).format('DD日 hh:mm');
           }
           break;
         case 'status':
-          return moment(time).format('MM月DD日 hh:mm');
+          return moment(time).tz(tz).format('MM月DD日 hh:mm');
         case 'week':
-          return moment(time).format('YYYY年MM月DD日 dd hh:mm');
+          return moment(time).tz(tz).format('YYYY年MM月DD日 dd hh:mm');
         default :
-          return moment(time).format('YYYY-MM-DD hh:mm:ss');
+          return moment(time).tz(tz).format('YYYY-MM-DD hh:mm:ss');
       }
     };
   });
